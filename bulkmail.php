78abc61c4f85283ed5b4e4d83fbc191e43f85db9
Suresh Kumar Khatua <35217724+sureshkumar0203@users.noreply.github.com>
2024-09-10 17:48:28 +0530

    Update bulkmail.php

--- bulkmail.php
+++ bulkmail.php
@@ -12,7 +12,7 @@ function sendAuthMailSkillDevelop($jsData){
 
     $mail->SMTPDebug = 2;
     $mail->isSMTP();
-    $mail->Host = 'apps.abc.gov.in';
+    $mail->Host = 'apps.abc.gg.in';
     $mail->Port = 25;
     $mail->SMTPAuth = true;
     $mail->SMTPSecure = 'tls';
@@ -21,7 +21,7 @@ function sendAuthMailSkillDevelop($jsData){
 
 
     $mail->From = '';
-    $mail->FromName = 'Skill Development Program';
+    $mail->FromName = 'test';
     $mail->addAddress($data->to, $data->name);            
     $mail->isHTML(true);
 
@@ -41,22 +41,22 @@ function sendAuthMailSkillDevelop($jsData){
 }
 
 $strUserFrom = '';
-$strsubject = "Invitation to Join OSDA-FITT IITD Orientation Program Test Run";
+$strsubject = "testing";
 
 $strUserMessage = "Dear <strong>Participant</strong>,</br></br>";
 $strUserMessage .= "<div> <br></div>";
 
 
-$strUserMessage .= "Thank you for registering for the OSDA-FITT IITD Orientation Program. We are excited to inform you that we will be conducting a test run of the orientation program to ensure everything operates smoothly during the main event.</br></br>";
+$strUserMessage .= "Thank you</br></br>";
 $strUserMessage .= "<div> <br>";
 $strUserMessage .= "</div>";
 
-$strUserMessage .= "We kindly request your participation in this test run, which will help us provide a seamless experience for everyone. Please find the details to join the test run session below:</br></br>";
+$strUserMessage .= "We kindly  :</br></br>";
 
 $strUserMessage .= "<div> <br>";
 $strUserMessage .= "</div>";
 
-$strUserMessage .= "<strong>Link:</strong> <a href='https://events.teams.microsoft.com/event/b7d346de-7a7f-4236-ac6b-45ed74534f22@c043f805-00ce-4f56-8770-bc3468f13ada'>Microsoft Teams Event</a></br>";
+$strUserMessage .= "<strong>Link:</strong> dfsf</br>";
 
 $strUserMessage .= "<div> <br>";
 $strUserMessage .= "</div>";
@@ -70,20 +70,20 @@ $strUserMessage .= "<strong>Time:</strong> 5:30 p.m. to 6:00 p.m.</br></br>";
 $strUserMessage .= "<div> <br>";
 $strUserMessage .= "</div>";
 
-$strUserMessage .= "Your participation and feedback will be invaluable in fine-tuning the program. Should you encounter any issues or have questions during the session, please do not hesitate to reach out to us at <a href='mailto:coe.osda.iitdelhi@gmail.com'>coe.osda.iitdelhi@gmail.com</a>.</br></br>";
+$strUserMessage .= "Your participation.</br></br>";
 
 $strUserMessage .= "<div> <br>";
 $strUserMessage .= "</div>";
 
 
-$strUserMessage .= "We look forward to your involvement.</br></br>";
+$strUserMessage .= "We look forward</br></br>";
 
 $strUserMessage .= "<div> <br>";
 $strUserMessage .= "</div>";
 
 $strUserMessage .= "<div>Thanks and regards,</br></div>";
-$strUserMessage .= "<div><strong>Skill Development Team</strong></br></div>";
-$strUserMessage .= "<div><strong>OSDA</strong></br></div>";
+$strUserMessage .= "<div><strong>ASD</strong></br></div>";
+$strUserMessage .= "<div><strong>FGH</strong></br></div>";
 
 //$emailArray = array('','');
 //print_r($emailArray);exit;
@@ -95,7 +95,7 @@ include_once("mailList.php");
 for($i=0; $i<count($emailArray); $i++){
     $userdata['from'] = $strUserFrom;
     $userdata['to'] = $emailArray[$i];
-    $userdata['name'] = 'Odisha Skill Development Authority';
+    $userdata['name'] = 'HELLO';
     $userdata['sub'] = $strsubject;
     $userdata['message'] = $strUserMessage;
 	
